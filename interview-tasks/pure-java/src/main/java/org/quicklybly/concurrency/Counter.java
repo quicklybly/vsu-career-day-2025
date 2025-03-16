830a53584d0abd9040a0335aa5e233eec061fd79
quicklybly <ar.r.lysenko@tinkoff.ru>
2025-03-16 22:01:56 +0300

    init interview pure java questions

---
+++ interview-tasks/pure-java/src/main/java/org/quicklybly/concurrency/Counter.java
@@ -0,0 +1,25 @@
+package org.quicklybly.concurrency;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class Counter {
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        var executor = Executors.newFixedThreadPool(100);
+        int targetCount = 100_000;
+
+        for (int i = 0; i < targetCount; i++) {
+            executor.submit(() -> {
+                count++;
+            });
+        }
+
+        executor.shutdown();
+        boolean result = executor.awaitTermination(10, TimeUnit.SECONDS);
+
+        System.out.println(count);
+        System.exit(result ? 0 : 1);
+    }
+}
