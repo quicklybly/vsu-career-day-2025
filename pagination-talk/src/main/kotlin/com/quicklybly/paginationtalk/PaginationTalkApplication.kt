f153b04746e83e3266720181a3cd9024db5d9c59
ar.r.lysenko <ar.r.lysenko@tinkoff.ru>
2025-03-16 18:47:34 +0300

    talk initial commit

---
+++ pagination-talk/src/main/kotlin/com/quicklybly/paginationtalk/PaginationTalkApplication.kt
@@ -0,0 +1,11 @@
+package com.quicklybly.paginationtalk
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaginationTalkApplication
+
+fun main(args: Array<String>) {
+	runApplication<PaginationTalkApplication>(*args)
+}
---
+++ pagination-talk/src/test/kotlin/com/quicklybly/paginationtalk/PaginationTalkApplicationTests.kt
@@ -0,0 +1,14 @@
+package com.quicklybly.paginationtalk
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaginationTalkApplicationTests {
+
+	@Test
+	fun contextLoads() {
+		println("all good")
+	}
+
+}
